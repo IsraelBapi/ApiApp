4f918e76ab140594d36e8bab2482ebb30fdc5883
IsraelBapi <israelbapi27@gmail.com>
2025-04-13 14:01:20 -0600

    Update server.js

--- server.js
+++ server.js
@@ -592,7 +592,7 @@ app.get('/api/sales/:sale_id/activities', (req, res) => {
     });
 });
 
-app.get('/salesUser/:userId', async (req, res) => {
+app.get('/salesUser/:userId',(req, res) => {
     const userId = req.params.userId;
 
     const query = `
